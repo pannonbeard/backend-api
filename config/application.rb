ebf741589af41f3ad446dcd737f54ba22298d7cf
Phil Hendricks <phendricks@aea267.k12.ia.us>
2018-03-16 14:30:53 -0500

    Added in cookies

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module BackendApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.autoload_paths << Rails.root.join('lib')
-    #config.middleware.use Rack::Session::Cookie, secret: Rails.application.secrets.secret_key_base
+    config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
