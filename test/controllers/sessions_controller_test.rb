1c311b409ebde71bee41a7abf4a8c32dd951ad9d
Phil Hendricks <phendricks@aea267.k12.ia.us>
2018-03-19 09:15:23 -0500

    testing callback function and destroy for sessions

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -6,9 +6,33 @@ require 'test_helper'
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
+    OmniAuth.config.test_mode = true
+
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
+      {
+        :provider => 'google_oauth2',
+        :uid => '52369',
+        'info' => {
+          'email' => @user.email,
+          'name' => @user.name
+        }
+      }
+    )
   end
 
   test 'should return true' do
     assert true
+  end
+
+  test 'should find user' do
+    get '/auth/google_oauth2/callback'
+    @request.env['omniauth.env'] = OmniAuth.config.mock_auth[:google_oauth2]
+
+    assert_response :ok
+  end
+
+  test 'should destroy session' do
+    get '/logout'
+    assert_redirected_to root_path
   end
 end
