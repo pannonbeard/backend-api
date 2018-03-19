63914ae02fca0b5b07b523c21c21eff14f332eab
Phil Hendricks <phendricks@aea267.k12.ia.us>
2018-03-19 08:35:02 -0500

    added setup to session test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,13 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 # Test login page redirects
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+
   test 'should return true' do
     assert true
   end
