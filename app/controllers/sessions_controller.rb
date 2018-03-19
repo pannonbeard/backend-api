2a1eee17a44197c3ba351465da1f3b6a9153de23
Phil Hendricks <phendricks@aea267.k12.ia.us>
2018-03-19 08:33:46 -0500

    refactored auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,14 @@
+# frozen_string_literal: true
+
 # Controls Login Call back
 class SessionsController < ApplicationController
   def create
     ## Call back from google that user is verified
     @user = User.find_from_omniauth(auth)
-    #render json: @user
 
     ## create json web token to send back to front end
-    if @user.email == 'phendricks@centralriversaea.org'
-      jwt = Auth.issue(
-        {
-          user: @user.id,
-          exp: experation
-        }
-      )
-      render json: { jwt: jwt }
-    end
+    jwt = Auth.issue(user: @user.id, exp: experation)
+    render json: { jwt: jwt }
   end
 
   def destroy
