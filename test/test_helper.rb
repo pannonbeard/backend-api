b2e2a0a498b4764e289055993a8ed0839517c498
Phil Hendricks <phendricks@aea267.k12.ia.us>
2018-03-19 08:33:56 -0500

    added simple cov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,17 @@
+# frozen_string_literal: true
+
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
+SimpleCov.start 'rails'
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+# for all the test
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  # Setup all fixtures in test/fixtures/*.yml
+  # for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
