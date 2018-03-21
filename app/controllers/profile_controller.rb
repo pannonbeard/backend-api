089f254cd6d15902d13a3cd92b788d9e266bdc85
Phil Hendricks <phendricks@aea267.k12.ia.us>
2018-03-21 08:08:51 -0500

    removed extra guff from profile for the time being

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -11,16 +11,6 @@ class ProfileController < ApplicationController
     }
   end
 
-  def new; end
-
-  def create; end
-
-  def edit; end
-
-  def update; end
-
-  def destroy; end
-
   private
 
   def profile_params; end
